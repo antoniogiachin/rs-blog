dd80ee420cebfb772ed68ba8820efe34dd151187
antoniogiachin <giachinantonio@outlook.com>
2022-10-14 09:40:30 +0200

    aggiornamento

---
+++ src/components/Dashoboard/DashboardDisplay.jsx
@@ -0,0 +1,3 @@
+export const DashboardDisplay = () => {
+  return <div>DashboardDisplay</div>;
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,14 +2,19 @@
 import { useState } from "react";
 // * Import Components
 import { DashboardActions } from "../components/Dashoboard/DashboardActions";
+import { DashboardDisplay } from "../components/Dashoboard/DashboardDisplay";
 
 export const Dashboard = () => {
   const [isActionSelected, setIsActionSelected] = useState(false);
+  const [actionSelected, setActionSelected] = useState("");
 
   return (
     <div>
       Dashboard
-      <DashboardActions isActionSelected={isActionSelected} />
+      <DashboardActions
+        isActionSelected={(isActionSelected, setActionSelected)}
+      />
+      <DashboardDisplay />
     </div>
   );
 };
