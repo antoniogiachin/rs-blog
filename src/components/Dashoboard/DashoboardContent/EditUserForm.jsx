ec1c9ce2da397aaeba26b58af9b4604632d07bd5
Antonio Giachin <giachinantonio@outlook.com>
2022-10-25 09:29:12 +0200

    inizio implementazione edit user

--- src/components/Dashoboard/DashoboardContent/EditUserForm.jsx
+++ src/components/Dashoboard/DashoboardContent/EditUserForm.jsx
@@ -11,16 +11,13 @@ import styles from "../../Login/RegisterForm.module.css";
 import { TheButton } from "../../UI/TheButton";
 import { TheBadge } from "../../UI/TheBadge";
 
-// * Import Hooks
-// import { useRegister } from "../../hooks/useRegister";
-
 // * Import Redux
 import { useDispatch, useSelector } from "react-redux";
 import { userInfosBatch } from "../../../store/slicers/authSlice";
+import { SET_ERROR, errorMessage } from "../../../store/slicers/errorSlice";
 
 // * Import FontAwasome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 export const EditUserForm = () => {
   // hook
@@ -32,9 +29,9 @@ export const EditUserForm = () => {
   // redux
   const dispatch = useDispatch();
   const userInfos = useSelector(userInfosBatch);
+  const error = useSelector(errorMessage);
 
   // regex email e password
-  const PWD_REGEX = /[0-9a-zA-Z]{6,}/;
   const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
   const nameRef = useRef();
@@ -44,44 +41,37 @@ export const EditUserForm = () => {
   const [email, setEmail] = useState(userInfos.email);
   const [isValidEmail, setIsValidEmail] = useState(false);
 
-  const [password, setPassword] = useState(userInfos.password);
-  const [isValidPassword, setIsValidPassword] = useState(false);
-
   const [surname, setSurname] = useState(userInfos.surname);
   const [username, setUsername] = useState(userInfos.username);
   const [birthDate, setBirthDate] = useState(userInfos.birthDate.split("T")[0]);
-  const [isAuthor, setIsAuthor] = useState(userInfos.isAuthor);
   const [profilePicture, setProfilePicture] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!isValidPassword) {
-      // dispatch(SET_ERROR("Password must be at least 6 character"));
-      return;
-    }
-
     if (!isValidEmail) {
-      // dispatch(SET_ERROR("Please provide a valid email"));
+      dispatch(
+        SET_ERROR({ status: 400, message: "Please provide a valid email" })
+      );
       return;
     }
 
-    const res = await handleRegister({
-      name,
-      surname,
-      username,
-      email,
-      password,
-      isAuthor,
-      birthDate,
-      profilePicture,
-    });
+    // const res = await handleRegister({
+    //   name,
+    //   surname,
+    //   username,
+    //   email,
+    //   password,
+    //   isAuthor,
+    //   birthDate,
+    //   profilePicture,
+    // });
 
-    if (res && res.success) {
-      navigate("/");
-    } else {
-      SET_IS_LOADING(false);
-    }
+    // if (res && res.success) {
+    //   navigate("/");
+    // } else {
+    //   SET_IS_LOADING(false);
+    // }
   };
 
   // al [] focus su nameRef
@@ -99,21 +89,12 @@ export const EditUserForm = () => {
   }, [email]);
 
   useEffect(() => {
-    const validation = PWD_REGEX.test(password);
-    if (validation && password) {
-      setIsValidPassword(true);
-    } else {
-      setIsValidPassword(false);
+    if (error) {
+      setTimeout(() => {
+        dispatch(SET_ERROR(null));
+      }, 5000);
     }
-  }, [password]);
-
-  // useEffect(() => {
-  //   if (error) {
-  //     setTimeout(() => {
-  //       dispatch(SET_ERROR(null));
-  //     }, 5000);
-  //   }
-  // }, [error]);
+  }, [error]);
 
   return (
     <form className="grid gap-4 grid-cols-2" onSubmit={handleSubmit}>
@@ -173,23 +154,7 @@ export const EditUserForm = () => {
           }}
         />
       </label>
-      {/* password */}
-      <label className={styles.ms_label}>
-        <span>password: </span>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <div className={!isValidPassword && password ? "block mt-2" : "hidden"}>
-          <FontAwesomeIcon icon={faExclamationCircle} className="mr-2" />
-          <span>
-            Password must be at least 6 characters (letters, numbers or simbols)
-          </span>
-        </div>
-      </label>
+
       {/* profilePicture  */}
       <label className="flex items-center justify-between  space-x-3">
         <span>profile picture: </span>
@@ -200,29 +165,14 @@ export const EditUserForm = () => {
           }}
         />
       </label>
-      {/* author checkbox  */}
-      <label className="flex items-center justify-end  space-x-3">
-        <span>Are you an author? </span>
-        <input
-          type="checkbox"
-          value={isAuthor}
-          onChange={(e) => {
-            setIsAuthor((prevState) => !prevState);
-          }}
-        />
-      </label>
 
       <div className="col-span-2 flex items-center  justify-end space-x-5 mt-2">
         <TheButton
           disabled={
-            !name ||
-            !surname ||
-            !username ||
-            !isValidEmail ||
-            !isValidPassword ||
-            !birthDate
+            !name || !surname || !username || !isValidEmail || !birthDate
           }
-          label="Register"
+          label="Save"
+          icon={faSave}
         />
       </div>
 
